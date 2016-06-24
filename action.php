35cad79268be8e2b877be727d6f84846ee2d123e
Andreas Gohr <andi@splitbrain.org>
2016-06-24 13:49:35 +0200

    cleanup and better way of passing data between stages

--- action.php
+++ action.php
@@ -9,52 +9,70 @@
 
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 
-require_once(DOKU_PLUGIN.'action.php');
+require_once(DOKU_PLUGIN . 'action.php');
 
+/**
+ * Class action_plugin_ditaa
+ */
 class action_plugin_ditaa extends DokuWiki_Action_Plugin {
-    
+
+    /**
+     * Registers a callback function for a given event
+     *
+     * @param Doku_Event_Handler $controller
+     */
     public function register(Doku_Event_Handler $controller) {
         // Download of a file
-        
+
         $controller->register_hook('MEDIA_SENDFILE', 'BEFORE', $this, 'ditaa_sendfile');
         $controller->register_hook('FETCH_MEDIA_STATUS', 'BEFORE', $this, 'ditaa_sendfile_not_found');
     }
 
-    /*
-     * Redirect File to real File
+    /**
+     * Handle Ditaa file requests
+     *
+     * @param Doku_Event $event
+     * @param $args
      */
-    function ditaa_sendfile(&$event, $args) {
+    public function ditaa_sendfile(Doku_Event $event, $args) {
         global $conf;
+        global $INPUT;
 
-        if ( empty( $_REQUEST['ditaa'] ) ) {
-            return;
-        }
-        
-        $plugin = plugin_load( 'syntax', 'ditaa' );
-        $data = p_get_metadata( $event->data['media'], 'ditaa' );
-        $event->data['file'] = $plugin->_imgfile($event->data['media'], $data[$_REQUEST['ditaa']]);
+        if(!$INPUT->str('ditaa')) return;
+
+        /** @var syntax_plugin_ditaa $plugin */
+        $plugin = plugin_load('syntax', 'ditaa');
+
+        $event->data['file'] = $plugin->_imgfile($INPUT->str('ditaa'));
         $event->data['mime'] = 'image/png';
-        
-        if( !$event->data['file'] ) {
+        $event->data['download'] = false;
+
+        if(!$event->data['file']) {
             $event->data['file'] = dirname(__FILE__) . '/broken.png';
             $event->data['status'] = 404;
             $event->data['statusmessage'] = 'Not Found';
         }
 
-        header('Expires: '.gmdate("D, d M Y H:i:s", time()+max($conf['cachetime'], 3600)).' GMT');
-        header('Cache-Control: public, proxy-revalidate, no-transform, max-age='.max($conf['cachetime'], 3600));
+        header('Expires: ' . gmdate("D, d M Y H:i:s", time() + max($conf['cachetime'], 3600)) . ' GMT');
+        header('Cache-Control: public, proxy-revalidate, no-transform, max-age=' . max($conf['cachetime'], 3600));
         header('Pragma: public');
     }
 
-    /*
+    /**
      * If a file has not been found yet, we should try to check if this can be solved
      * via the ditaa renderer
+     *
+     * @param Doku_Event $event
+     * @param $args
+     * @return bool
      */
-    function ditaa_sendfile_not_found(&$event, $args)
-    {
-        if ( $event->data['status'] >= 500 || empty( $_REQUEST['ditaa'] ) ) { return true; }
+    public function ditaa_sendfile_not_found(Doku_Event $event, $args) {
+        global $INPUT;
+        if($event->data['status'] >= 500 || !$INPUT->str('ditaa')) {
+            return true;
+        }
         $event->data['status'] = 200;
         $event->data['statusmessage'] = 'OK';
         return true;
--- syntax.php
+++ syntax.php
@@ -14,41 +14,52 @@ if(!defined('DOKU_INC')) define('DOKU_INC', realpath(dirname(__FILE__) . '/../..
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 require_once(DOKU_PLUGIN . 'syntax.php');
 
+/**
+ * Class syntax_plugin_ditaa
+ */
 class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
 
     /**
      * What about paragraphs?
      */
-    function getPType() {
+    public function getPType() {
         return 'normal';
     }
 
     /**
      * What kind of syntax are we?
      */
-    function getType() {
+    public function getType() {
         return 'substition';
     }
 
     /**
      * Where to sort in?
      */
-    function getSort() {
+    public function getSort() {
         return 200;
     }
 
     /**
      * Connect pattern to lexer
+     *
+     * @param string $mode
      */
-
-    function connectTo($mode) {
+    public function connectTo($mode) {
         $this->Lexer->addSpecialPattern('<ditaa.*?>\n.*?\n</ditaa>', $mode, 'plugin_ditaa');
     }
 
     /**
-     * Handle the match
+     * Stores all infor about the diagram in two files. One is the actual ditaa data, the other
+     * contains the options.
+     *
+     * @param   string $match The text matched by the patterns
+     * @param   int $state The lexer state for the match
+     * @param   int $pos The character position of the matched text
+     * @param   Doku_Handler $handler The Doku_Handler object
+     * @return  bool|array Return an array with all data you want to use in render, false don't add an instruction
      */
-    function handle($match, $state, $pos, Doku_Handler $handler) {
+    public function handle($match, $state, $pos, Doku_Handler $handler) {
         $info = $this->getInfo();
 
         // prepare default data
@@ -88,50 +99,24 @@ class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
         $input = join("\n", $lines);
         $return['md5'] = md5($input); // we only pass a hash around
 
-        // store input for later use
-        io_saveFile($this->_cachename($return, 'txt'), $input);
+        // store input for later use in _imagefile()
+        io_saveFile(getCacheName($return['md5'], '.ditaa.txt'), $input);
+        io_saveFile(getCacheName($return['md5'], '.ditaa.cfg'), serialize($return));
 
         return $return;
     }
 
     /**
-     * Prepares the Data that is used for the cache name
-     * Width, height and scale are left out.
-     * Ensures sanity.
+     * Output the image
+     *
+     * @param string $format output format being rendered
+     * @param Doku_Renderer $R the current renderer object
+     * @param array $data data created by handler()
+     * @return  boolean                 rendered correctly?
      */
-    function _prepareData($input) {
-        $output = array();
-        foreach($input as $key => $value) {
-            switch($key) {
-                case 'scale':
-                case 'antialias':
-                case 'edgesep':
-                case 'round':
-                case 'shadow':
-                case 'md5':
-                    $output[$key] = $value;
-            };
-        }
-
-        ksort($output);
-        return $output;
-    }
-
-    /**
-     * Cache file is based on parameters that influence the result image
-     */
-    function _cachename($data, $ext) {
-        $data = $this->_prepareData($data);
-        return getcachename(join('x', array_values($data)), '.ditaa.' . $ext);
-    }
-
-    /**
-     * Create output
-     */
-    function render($format, Doku_Renderer $R, $data) {
+    public function render($format, Doku_Renderer $R, $data) {
         global $ID;
         if($format == 'xhtml') {
-
             // Only use the md5 key
             $img = ml($ID, array('ditaa' => $data['md5']));
             $R->doc .= '<img src="' . $img . '" class="media' . $data['align'] . '" alt=""';
@@ -142,12 +127,9 @@ class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
             $R->doc .= '/>';
             return true;
         } else if($format == 'odt') {
-            $src = $this->_imgfile($data);
+            $src = $this->_imgfile($data['md5']);
+            /** @var  renderer_plugin_odt $R */
             $R->_odtAddImage($src, $data['width'], $data['height'], $data['align']);
-            return true;
-        } else if($format == 'metadata') {
-            // Save for later use
-            $R->meta['ditaa'][$data['md5']] = $data;
             return true;
         }
         return false;
@@ -155,45 +137,55 @@ class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
 
     /**
      * Return path to the rendered image on our local system
+     *
+     * @param string $md5 MD5 of the input data, used to identify the cache files
+     * @return false|string path to file or fals on error
      */
-    function _imgfile($id, $data, $secondTry = false) {
+    public function _imgfile($md5) {
+        $file_cfg = getCacheName($md5, '.ditaa.cfg'); // configs
+        $file_txt = getCacheName($md5, '.ditaa.txt'); // input
+        $file_png = getCacheName($md5, '.ditaa.png'); // ouput
 
-        $cache = $this->_cachename($data, 'png');
+        if(!file_exists($file_cfg) || !file_exists($file_txt)) {
+            return false;
+        }
+        $data = unserialize(io_readFile($file_cfg, false));
 
-        // create the file if needed
-        if(!file_exists($cache)) {
-            $in = $this->_cachename($data, 'txt');
-            // If this is nt yet here, force geting instructions and writing the thing back.
-            if($secondTry != true && !file_exists($in)) {
-                p_get_instructions(io_readFile(wikiFN($id)));
-                return $this->_imgfile($id, $data, true);
-            }
+        // file does not exist or is outdated
+        if(@filemtime($file_png) < filemtime($file_cfg)) {
 
             if($this->getConf('java')) {
-                $ok = $this->_run($data, $in, $cache);
+                $ok = $this->_runJava($data, $file_txt, $file_png);
             } else {
-                $ok = $this->_runGo($data, $in, $cache);
+                $ok = $this->_runGo($data, $file_txt, $file_png);
                 #$ok = $this->_remote($data, $in, $cache);
             }
             if(!$ok) return false;
-            clearstatcache();
+
+            clearstatcache($file_png);
         }
 
         // resized version
         if($data['width']) {
-            $cache = media_resize_image($cache, 'png', $data['width'], $data['height']);
+            $file_png = media_resize_image($file_png, 'png', $data['width'], $data['height']);
         }
 
         // something went wrong, we're missing the file
-        if(!file_exists($cache)) return false;
+        if(!file_exists($file_png)) return false;
 
-        return $cache;
+        return $file_png;
     }
 
     /**
      * Render the output remotely at ditaa.org
+     *
+     * @deprecated ditaa.org is no longer available, so this defunct
+     * @param array $data The config settings
+     * @param string $in Path to the ditaa input file (txt)
+     * @param string $out Path to the output file (PNG)
+     * @return bool true if the image was created, false otherwise
      */
-    function _remote($data, $in, $out) {
+    protected function _remote($data, $in, $out) {
         global $conf;
 
         if(!file_exists($in)) {
@@ -223,8 +215,13 @@ class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
 
     /**
      * Run the ditaa Java program
+     *
+     * @param array $data The config settings
+     * @param string $in Path to the ditaa input file (txt)
+     * @param string $out Path to the output file (PNG)
+     * @return bool true if the image was created, false otherwise
      */
-    function _run($data, $in, $out) {
+    protected function _runJava($data, $in, $out) {
         global $conf;
 
         if(!file_exists($in)) {
@@ -260,8 +257,13 @@ class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
 
     /**
      * Run the ditaa Go program
+     *
+     * @param array $data The config settings - currently not used because the Go relase supports no options
+     * @param string $in Path to the ditaa input file (txt)
+     * @param string $out Path to the output file (PNG)
+     * @return bool true if the image was created, false otherwise
      */
-    function _runGo($data, $in, $out) {
+    protected function _runGo($data, $in, $out) {
         global $conf;
 
         if(!file_exists($in)) {
@@ -331,6 +333,8 @@ class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
     /**
      * Returns the local binary to use
      *
+     * Downloads it if necessary
+     *
      * @return bool|string
      */
     protected function getLocalBinary() {
@@ -340,8 +344,8 @@ class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
         if(!$bin) return false;
 
         // check distributed files first
-        if(file_exists(__DIR__.'/ditaa/'.$bin)) {
-            return __DIR__.'/ditaa/'.$bin;
+        if(file_exists(__DIR__ . '/ditaa/' . $bin)) {
+            return __DIR__ . '/ditaa/' . $bin;
         }
 
         $info = $this->getInfo();
@@ -349,7 +353,7 @@ class syntax_plugin_ditaa extends DokuWiki_Syntax_Plugin {
 
         if(file_exists($cache)) return $cache;
 
-        $url = 'https://github.com/splitbrain/dokuwiki-plugin-ditaa/raw/bins/'.$bin;
+        $url = 'https://github.com/splitbrain/dokuwiki-plugin-ditaa/raw/bins/' . $bin;
         if(io_download($url, $cache, false, '', 0)) {
             @chmod($cache, $conf['dmode']);
             return $cache;
